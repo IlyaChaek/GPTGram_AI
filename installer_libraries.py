d41a5c2b0463ca3c15b09ff4dac1e0fabf35b62d
Chorelin <miheevila6@gmail.com>
2025-03-19 18:44:34 +0300

    добавлена функция обновления pip
    перед установкой библиотек будет установлен последний доступный pip

--- installer_libraries.py
+++ installer_libraries.py
@@ -1,16 +1,22 @@
 import subprocess
+
 def install_requirements():
     try:
+        print('Обновляю pip до последней версии...')
+        subprocess.check_call(['python', '-m', 'pip', 'install', '--upgrade', 'pip'])
+        print('pip успешно обновлён\n')
+
         with open('requirements.txt', 'r') as file:
             packages = file.read().splitlines()
             for package in packages:
                 if package.strip() and not package.startswith('#'):
                     print(f'Устанавливаю {package}...')
                     subprocess.check_call(['pip', 'install', package])
-        print('Все зависимости установлены')
+        print('\nВсе зависимости установлены')
     except FileNotFoundError:
         print('Файл requirements.txt не найден')
     except subprocess.CalledProcessError as e:
         print(f'Ошибка при установке пакета: {e}')
+        
 if __name__ == '__main__':
     install_requirements()
